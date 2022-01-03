ff37dd43cb3158b8eb1d316f1412a106057dbe20
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-03 18:50:21 +0000

    MVP completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMcontentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const newItemform = document.querySelector('#new-item-form');
     newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
